1781a09ee529fc0d922c7e4b672e879528adef83
iganari <cigaguri@gmail.com>
2020-09-24 15:26:51 +0900

    初期配置作成

---
+++ helloworld-go-custom/go.mod
@@ -0,0 +1,4 @@
+module github.com/knative/docs/docs/serving/samples/hello-world/helloworld-go
+
+go 1.13
+
---
+++ helloworld-go-custom/helloworld-custom.go
@@ -0,0 +1,11 @@
+// https://github.com/knative/docs/blob/master/docs/serving/samples/hello-world/helloworld-go/helloworld.go
+// https://qiita.com/nyamage/items/80ca5480baad8da581df
+
+pacakge main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
